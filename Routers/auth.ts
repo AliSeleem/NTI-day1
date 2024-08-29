379f1388fd5054a37db360d2c4fc9d013c71076d
Ali Seleem <engaliseleem@gmail.com>
2024-08-29 05:00:55 +0300

    Update auth.ts to add validators

--- Routers/auth.ts
+++ Routers/auth.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { login, signup } from "../Controllers/auth";
+import {loginValidator, signupValidator} from"../utils/validation/authValidator"
 const authRoute = Router();
 
-authRoute.route("/signup").post(signup);
-authRoute.route("/login").post(login);
+authRoute.route("/signup").post(signupValidator, signup);
+authRoute.route("/login").post(loginValidator, login);
 export default authRoute;
